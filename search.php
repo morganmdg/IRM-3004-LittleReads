9cae4ff1e3fd88775955a8afb73cc367434b2151
jacobabr9 <jacobabr9@gmail.com>
2024-03-21 18:19:13 -0400

    Lint Fix
    Resolving issues found from linter

--- search.php
+++ search.php
@@ -1,4 +1,13 @@
 <?php
+/** @psalm-suppress all */
+
+/**
+ * littlereads-login.php
+ * This file handles user sign-up and sign-in functionality.
+ * @author Dominic Murphy
+ * @version 1.0
+ * @date February 27, 2024
+ */
 // Database connection
 $servername = "localhost";
 $username = "pma";
@@ -16,17 +25,17 @@ if ($conn->connect_error) {
 // Construct the SQL query based on the selected genres
 $sql = "SELECT Title, Genres, ImageLink, ISBN FROM book WHERE 1=1";
 
-if(isset($_GET['genre1']) && !empty($_GET['genre1'])) {
+if (isset($_GET['genre1']) && !empty($_GET['genre1'])) {
     $genre1 = $_GET['genre1'];
     $sql .= " AND Genres LIKE '%$genre1%'";
 }
 
-if(isset($_GET['genre2']) && !empty($_GET['genre2'])) {
+if (isset($_GET['genre2']) && !empty($_GET['genre2'])) {
     $genre2 = $_GET['genre2'];
     $sql .= " AND Genres LIKE '%$genre2%'";
 }
 
-if(isset($_GET['genre3']) && !empty($_GET['genre3'])) {
+if (isset($_GET['genre3']) && !empty($_GET['genre3'])) {
     $genre3 = $_GET['genre3'];
     $sql .= " AND Genres LIKE '%$genre3%'";
 }
@@ -39,12 +48,13 @@ $result = $conn->query($sql);
 // Check if any books were found
 if ($result->num_rows > 0) {
     // Output data of each row
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
         echo "<div class='book'>";
         echo "<h2>" . $row["Title"] . "</h2>";
         echo "<p>Genres: " . $row["Genres"] . "</p>";
         echo "<div class='image'>";
-        echo "<img src='" . $row["ImageLink"] . "' alt='" . $row["Title"] . "' onclick='goToDescription(\"" . $row["ISBN"] . "\")'>";
+        echo "<img src='" . $row["ImageLink"] . "' alt='" . 
+        $row["Title"] . "' onclick='goToDescription(\"" . $row["ISBN"] . "\")'>";
         echo "</div>";
         echo "</div>";
     }
@@ -56,11 +66,12 @@ if ($result->num_rows > 0) {
     // Check if any books were found
     if ($result->num_rows > 0) {
         // Output data of each row
-        while($row = $result->fetch_assoc()) {
+        while ($row = $result->fetch_assoc()) {
             echo "<div class='book'>";
             echo "<h2>" . $row["Title"] . "</h2>";
             echo "<p>Genres: " . $row["Genres"] . "</p>";
-            echo "<img src='" . $row["ImageLink"] . "' alt='" . $row["Title"] . "' onclick='goToDescription(\"" . $row["ISBN"] . "\")'>";
+            echo "<img src='" . $row["ImageLink"] . "' alt='" .
+            $row["Title"] . "' onclick='goToDescription(\"" . $row["ISBN"] . "\")'>";
             echo "</div>";
         }
     } else {
